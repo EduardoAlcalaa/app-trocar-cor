14b8e8996aa2cb97f8820409d339aa3575dbe3bf
EduardoAlcalaa <eduardoantunes755@gmail.com>
2024-09-02 16:37:38 -0300

    troca-cor

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useState } from 'react';
+import { View, Button, StyleSheet, SafeAreaView, Dimensions } from 'react-native';
 
-export default function App() {
+// Função para gerar uma cor aleatória
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
+
+  const handlePress = () => {
+    setBackgroundColor(getRandomColor());
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, { backgroundColor }]}>
+      <View style={styles.buttonContainer}>
+        <Button title="Trocar Cor" onPress={handlePress} />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+export default App;
